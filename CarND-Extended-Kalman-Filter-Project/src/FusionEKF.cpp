52ebedd32bbfdb1492d72d7a1ea7d47979fc57a8
Wagdi BEN YAALA <wb@generationrobots.com>
2019-07-24 08:30:13 +0200

    moving on with implementation

--- CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp
@@ -67,14 +67,22 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      // TODO: Initialize state.
-
+      ekf_.x_ << measurement_pack.raw_measurements_[0], 
+                    measurement_pack.raw_measurements_[1], 
+                    0, 
+                    0;
     }
 
     // done initializing, no need to predict or update
+    previous_timestamp_ = measurement_pack.timestamp_;    
     is_initialized_ = true;
     return;
   }
+
+  // compute the time elapsed between the current and previous measurements
+  // dt - expressed in seconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;  
 
   /**
    * Prediction
@@ -86,6 +94,28 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * TODO: Update the process noise covariance matrix.
    * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+
+  float noise_ax = 9;
+  float noise_ay = 9;
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+  /*cout << "F is : " << kf_.F_ << endl;*/
+  // 2. Set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << 1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
+  ekf_.Q_( 0, 0) = (pow(dt, 4) / 4) * noise_ax;
+  ekf_.Q_( 1, 1) = (pow(dt, 4) / 4) * noise_ay;
+  ekf_.Q_( 2, 2) = pow(dt, 2) * noise_ax;
+  ekf_.Q_( 3, 3) = pow(dt, 2) * noise_ay;
+  ekf_.Q_( 2, 0) = (pow(dt, 3) / 2) * noise_ax;
+  ekf_.Q_( 3, 1) = (pow(dt, 3) / 2) * noise_ay;
+  ekf_.Q_( 0, 2) = ekf_.Q_( 2, 0);
+  ekf_.Q_( 1, 3) = ekf_.Q_( 3, 1);
+
+  // 3. Call the Kalman Filter predict() function
 
   ekf_.Predict();
 
@@ -101,10 +131,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-
+    MatrixXd Hj = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;    
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // TODO: Laser updates
-
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- CarND-Extended-Kalman-Filter-Project/src/tools.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/tools.cpp
@@ -4,6 +4,8 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using std::cout;
+using std::endl;
 
 Tools::Tools() {}
 
@@ -11,14 +13,58 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+	// accumulate squared residuals
+	for (unsigned int i=0; i < estimations.size(); ++i) {
+		VectorXd residual = estimations[i] - ground_truth[i];
+		// coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	// calculate the mean
+	rmse = rmse/estimations.size();
+
+	// calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	// return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+	MatrixXd Hj(3,4);
+	// recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	// TODO: YOUR CODE HERE 
+	float sum_px_py = pow(px,2) + pow(py,2);
+	// check division by zero
+	if (sum_px_py == 0){
+	  	return Hj;
+	}
+	// compute the Jacobian matrix
+	Hj(0, 0) = px / sqrt(sum_px_py);
+	Hj(0, 1) = py / sqrt(sum_px_py);
+	Hj(1, 0) = -py / sum_px_py ;
+	Hj(1, 1) = px / sum_px_py;    
+	Hj(2, 0) = py * (vx*py-vy*px) / pow(sum_px_py, 3/2);
+	Hj(2, 1) = px * (-vx*py+vy*px) / pow(sum_px_py, 3/2);       
+	Hj(2, 2) = px / sqrt(sum_px_py);  
+	Hj(2, 3) = py / sqrt(sum_px_py); 
+
+	return Hj;
 }
